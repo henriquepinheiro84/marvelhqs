1974325e95c795003a94118783ebc5498953c767
Henrique Pinheiro <henri.pinheiro.84@gmail.com>
2024-07-18 11:16:06 -0300

    Pagination implemented.

--- MarvelHQs2/app/src/main/java/com/pinheiro/marvelhqs/presenter/MainActivity.kt
+++ MarvelHQs2/app/src/main/java/com/pinheiro/marvelhqs/presenter/MainActivity.kt
@@ -4,23 +4,40 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.pinheiro.marvelhqs.domain.viewobject.ComicViewObject
-import com.pinheiro.marvelhqs.ui.theme.MarvelHQsTheme
+import com.pinheiro.marvelhqs.presenter.ui.comic.ComicScreen
+import com.pinheiro.marvelhqs.presenter.ui.theme.MarvelHQsTheme
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
 import org.koin.androidx.compose.koinViewModel
 
 class MainActivity : ComponentActivity() {
@@ -32,39 +49,29 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             MarvelHQsTheme {
-                // A surface container using the 'background' color from the theme
-//                val factory = KoinViewModelFactory(LocalContext.current, )
-                val viewModel: MarvelViewModel = koinViewModel()
-                val comics = viewModel.comics.collectAsState(initial = null)
-                viewModel.getCharacters()
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.spacedBy(16.dp),
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    comics.value?.let {comics ->
-                        items(comics)  {comic ->
-                            comic.id?.let {
-                                ComicItem(
-                                    comic = comic,
-                                    modifier = Modifier.fillMaxWidth()
-                                    )
-                            }
-//                            if (it != null) {
-//                                ComicItem(
-//                                    comic = it,
-//                                    modifier = Modifier.fillMaxWidth()
-//                                )
-//                            }
-                        }
-
-                    }
-                }
+                ComicScreen()
 
             }
         }
     }
 }
+
+//@Composable
+//fun NavigationBarTest() {
+//    var selectedItem by remember { mutableIntStateOf(0) }
+//    val items = listOf("Songs", "Artists", "Playlists")
+//
+//    NavigationBar {
+//        items.forEachIndexed { index, item ->
+//            NavigationBarItem(
+//                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+//                label = { Text(item) },
+//                selected = selectedItem == index,
+//                onClick = { selectedItem = index }
+//            )
+//        }
+//    }
+//}
 
 @Composable
 fun toast() {
